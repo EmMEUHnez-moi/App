2741d945a631ad76b93e2b557dbe57535f116725
William Machecourt <williamf1@orange.fr>
2025-03-20 10:34:14 +0100

    request API for connexion

--- lib/profil/view/connexion_view.dart
+++ lib/profil/view/connexion_view.dart
@@ -1,11 +1,15 @@
+import 'package:emmeuhnez_moi_app/profil/cubit/login_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:emmeuhnez_moi_app/accueil/widget/button_accueil.dart';
 import 'package:emmeuhnez_moi_app/trajets/widget/champforumlaire_picker.dart';
 import 'package:emmeuhnez_moi_app/main.dart';
 import 'package:emmeuhnez_moi_app/profil/view/register_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ConnexionView extends StatelessWidget {
-  const ConnexionView({super.key});
+  ConnexionView({super.key});
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -16,8 +20,16 @@ class ConnexionView extends StatelessWidget {
         body: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(children: <Widget>[
-              ChampFormulaire(label: "Identifiant ou mail", texteduchamp: ''),
-              ChampFormulaire(label: "Mot de passe", texteduchamp: ''),
+              ChampFormulaire(
+                label: "Identifiant ou mail",
+                texteduchamp: '',
+                controller: _emailController,
+              ),
+              ChampFormulaire(
+                label: "Mot de passe",
+                texteduchamp: '',
+                controller: _passwordController,
+              ),
               Center(
                 child: GestureDetector(
                     onTap: () {
@@ -29,14 +41,29 @@ class ConnexionView extends StatelessWidget {
                     child: Text(
                         "Vous n'avez pas de compte, inscrivez-vous en cliquant ici")),
               ),
-              Center(
-                  child: CustomButton(
-                label: 'Connexion',
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => MyHomePage()));
-                },
-              ))
+              BlocConsumer<LoginBloc, LoginState>(listener: (context, state) {
+                if (state is LoginSuccess) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("Connexion réussie !")));
+                } else if (state is LoginFailure) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("Erreur : ${state.error}")));
+                }
+              }, builder: (context, state) {
+                if (state is LoginLoading) {
+                  return CircularProgressIndicator();
+                }
+                return ElevatedButton(
+                  onPressed: () {
+                    final email = _emailController.text;
+                    final password = _passwordController.text;
+                    context
+                        .read<LoginBloc>()
+                        .add(LoginSubmitted(email: email, password: password));
+                  },
+                  child: Text("Connexion"),
+                );
+              }),
             ])));
   }
 }
--- lib/trajets/widget/champforumlaire_picker.dart
+++ lib/trajets/widget/champforumlaire_picker.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 class ChampFormulaire extends StatelessWidget {
   final String label;
   final String texteduchamp;
+  final TextEditingController? controller;
 
   const ChampFormulaire(
-      {super.key, required this.label, required this.texteduchamp});
+      {super.key,
+      required this.label,
+      required this.texteduchamp,
+      this.controller});
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +18,7 @@ class ChampFormulaire extends StatelessWidget {
         children: <Widget>[Text(label)],
       ),
       TextField(
+        controller: controller,
         decoration: InputDecoration(
             labelText: texteduchamp, border: OutlineInputBorder()),
       )
